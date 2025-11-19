44c486a1cc4224f785d6d8675756db0f53fffca3
Navid Dezashibi <dezashibi@gmail.com>
2025-11-19 15:12:10 +0400

    add new field to Items table through migrations

--- AspNetPortfolioApp1/Models/Item.cs
+++ AspNetPortfolioApp1/Models/Item.cs
@@ -1,7 +1,8 @@
-namespace AspNetPortfolioApp1.Models;
+﻿namespace AspNetPortfolioApp1.Models;
 
 public class Item
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public double Price { get; set; }
 }

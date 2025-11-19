1c0c20a7eed9325fbb47e8559cd76c855709aed8
Navid Dezashibi <dezashibi@gmail.com>
2025-11-19 15:03:15 +0400

    add database with example migration

---
+++ AspNetPortfolioApp1/Data/PortfolioApp1Context.cs
@@ -0,0 +1,9 @@
+﻿using AspNetPortfolioApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetPortfolioApp1.Data;
+
+public class PortfolioApp1Context(DbContextOptions<PortfolioApp1Context> options) : DbContext(options)
+{
+    public DbSet<Item> Items { get; set; }
+}
